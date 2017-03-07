686149c393341034221101a10dc5854ca7bc18b2
Will Chen <willchenyh@gmail.com>
2017-03-06 16:08:26 -0800

    edited conv

--- COGS181/hw4/conve_net_v1.py
+++ COGS181/hw4/conve_net_v1.py
@@ -11,6 +11,10 @@ mnist = get_mnist.read_data_sets("MNIST-data/", one_hot=True)
 
 x = tf.placeholder(tf.float32, [None, 784])
 
+# parameters
+lr = 0.001
+batch_size = 50
+
 
 # from here continued
 
@@ -79,20 +83,23 @@ y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.initialize_all_variables())
-for i in range(20000):
-  batch = mnist.train.next_batch(100)
-  if i%100 == 0:
-    train_accuracy = accuracy.eval(feed_dict={
+
+total_batch = int(mnist.train.num_examples/batch_size)
+for epoch in range(200):
+  for i in range(total_batch):
+    batch = mnist.train.next_batch(batch_size)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+  train_accuracy = accuracy.eval(feed_dict={
         x:batch[0], y_: batch[1], keep_prob: 1.0})
-    print("step %d, training accuracy %g"%(i, train_accuracy))
-    test_accuracy = accuracy.eval(feed_dict={
+  print("step %d, training accuracy %g"%(i, train_accuracy))
+  test_accuracy = accuracy.eval(feed_dict={
 	x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
-    print("test accuracy %g"%(test_accuracy))
-  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+  print("test accuracy %g"%(test_accuracy))
+
 
 
 
